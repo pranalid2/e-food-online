600fce330f956cd8852defb2777b196246b95529
pranalid2 <dhanawadepranali8@gmail.com>
2022-10-18 18:55:27 +0530

    updated

--- e_food/settings.py
+++ e_food/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main/static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
