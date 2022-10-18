54c0626e695c111404fab2f339e7e593042f90bc
pranalid2 <dhanawadepranali8@gmail.com>
2022-10-18 20:14:38 +0530

    fixed

--- e_food/settings.py
+++ e_food/settings.py
@@ -126,10 +126,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'main/static/')
+STATICFILES_DIRS =  [
+    os.path.join(BASE_DIR, 'static')
+]
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'main/static')
-# ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
