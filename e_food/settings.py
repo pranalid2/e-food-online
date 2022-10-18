b5e76139ab3f10efd5e59a803d895f59b41d9c06
pranalid2 <dhanawadepranali8@gmail.com>
2022-10-18 19:12:38 +0530

    updated

--- e_food/settings.py
+++ e_food/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'main/static/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'main/static')
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
