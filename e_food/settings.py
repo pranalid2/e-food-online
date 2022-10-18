9abb2dc992653797a9680d55f911dc0654e625d5
pranalid2 <dhanawadepranali8@gmail.com>
2022-10-18 19:50:55 +0530

    fixed

--- e_food/settings.py
+++ e_food/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'main/static/')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'main/static')
 # ]
